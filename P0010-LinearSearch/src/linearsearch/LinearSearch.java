1ec43b9b7d9918393ab89af075e92c960d22df8d
SadHilarious <188009195+SadHilarious@users.noreply.github.com>
2026-01-21 19:49:54 +0700

    Sửa comment sang tiếng anh

--- P0010-LinearSearch/src/linearsearch/LinearSearch.java
+++ P0010-LinearSearch/src/linearsearch/LinearSearch.java
@@ -12,15 +12,15 @@ public class LinearSearch {
         // loop until user inputs a valid positive integer
         while (true) {
             try {
-                System.out.println(msg); // in ra thông báo
+                System.out.println(msg); // print message 
                 String input = scanner.nextLine();
-                int num = Integer.parseInt(input); // ép đổi sang dạng int
-                if (num > 0) { // kiểm tra điều kiện số có âm hay ko 
+                int num = Integer.parseInt(input); // force to use int 
+                if (num > 0) { // check condition
                     return num;
                 } else {
                     System.out.println("Negative integer is NOT allowed!");
                 }
-            } catch (Exception e) { // Phát hiện lỗi => bắt user nhập lại
+            } catch (Exception e) { // tt helps detect all data type except INT
                 System.out.println("Please input a integer");
             }
         }
@@ -28,27 +28,27 @@ public class LinearSearch {
 
     public int[] generateRandomArray(int size) {
         Random random = new Random();
-        int[] arr = new int[size]; // Khởi tạo biến rỗng 
-        for (int i = 0; i < arr.length; i++) { // duyệt qua tất cả phân tử trong mảng
-            arr[i] = random.nextInt(size) + 1; // random ra từ 0 -> size -1, sau đó +1 => thành từ 1 -> size
+        int[] arr = new int[size]; // create empty variable for storring array
+        for (int i = 0; i < arr.length; i++) { // pass through all the INT in array
+            arr[i] = random.nextInt(size) + 1; // random ra từ 0 -> size -1, sau đó +1 => thành từ 1 -> size (méo bt dịch sang tiếng anh như lào)
         }
         return arr;
     }
 
     public void displayArray(int[] arr) {
-        System.out.println("The array: " + Arrays.toString(arr)); // làm này cho nhanh
+        System.out.println("The array: " + Arrays.toString(arr)); 
     }
 
     public void displaySearchResult(int[] numberArray, int searchValue) {
         boolean isFound = false;
-        for (int i = 0; i < numberArray.length; i++) { // duyệt qua tất cả phân tử trong mảng
-            if (numberArray[i] == searchValue) {// nếu đúng => in ra output
+        for (int i = 0; i < numberArray.length; i++) { // pass through all in INT in array
+            if (numberArray[i] == searchValue) {// if true => print bellow
                 System.out.println("Found " + searchValue + " at index: " + i);
-                isFound = true; // gắn cờ xác nhận đã tìm dc
-                break; 
+                isFound = true; // mark this INT as true
+                break; // stop if found
             }
         }
-        if (!isFound) { // nếu chưa tìm dc, in ra dòng dưới 
+        if (!isFound) { // if not found, print bellow
             System.out.println("Can't find index!");
         }
     }
